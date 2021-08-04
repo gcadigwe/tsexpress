66b05202f946c0befefd6051b832b0c9d9fd34e3
Sarzy <adigwegodswillchinonso@gmail.com>
2021-08-04 22:02:29 +0100

    form post route

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,17 @@
 import express, { Request, Response } from 'express';
+import { router } from './routes/loginRoutes';
+import bodyParser from 'body-parser';
 
 const app = express();
 
-app.get('/', (req: Request, res: Response) => {
-  res.send(`<div> 
-    <h1>Hi there!</h1>
-  </div>`);
-});
+app.use(router);
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// app.get('/', (req: Request, res: Response) => {
+//   res.send(`<div>
+//     <h1>Hi there!</h1>
+//   </div>`);
+// });
 
 app.listen(3000, () => {
   console.log('port is running');
