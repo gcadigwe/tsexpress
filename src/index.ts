3658705dc1b47fad8aa8ada4da7d676c04b587fe
Sarzy <adigwegodswillchinonso@gmail.com>
2021-08-04 21:33:34 +0100

    initialization

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
-console.log('like');
+import express, { Request, Response } from 'express';
+
+const app = express();
+
+app.get('/', (req: Request, res: Response) => {
+  res.send(`<div> 
+    <h1>Hi there!</h1>
+  </div>`);
+});
+
+app.listen(3000, () => {
+  console.log('port is running');
+});
